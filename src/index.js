b7285561d6738b79c6a31465612b71e12fe59ebf
Daniel Tsonkov <daniel_tzonkov@abv.bg>
2025-08-20 10:06:40 +0300

    up

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("asdasda!");
+});
+
 app.listen(port, () => {
   console.log(`server runing on port ${port}`);
 });
